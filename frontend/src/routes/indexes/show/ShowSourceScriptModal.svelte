a3adedc6f79ce58da681a4470559ec5db7fae1b5
Ayr2017 <ayrat.2013@ya.ru>
2025-12-19 16:24:42 +0300

    add test_command with subcommand execution to CLI app

--- frontend/src/routes/indexes/show/ShowSourceScriptModal.svelte
+++ frontend/src/routes/indexes/show/ShowSourceScriptModal.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-    import { Button, Modal, type ModalProps, P } from "flowbite-svelte";
+    import {Button, Modal, type ModalProps, P} from "flowbite-svelte";
+    import hljs from 'highlight.js';
+
 
     let openSourceQueryModal = $state(false);
     let {queryText} = $props();
+    const highlightedCode = hljs.highlight(
+        queryText,
+        { language: 'sql' }
+    ).value
 
     function onclick() {
         openSourceQueryModal = true;
@@ -13,8 +19,9 @@
 
 <Modal form title="Query" bind:open={openSourceQueryModal} size="xl">
     <P>
-        {queryText}
+        {@html highlightedCode}
     </P>
+
     {#snippet footer()}
         <Button type="submit">I accept</Button>
         <Button type="submit" color="alternative">Decline</Button>
